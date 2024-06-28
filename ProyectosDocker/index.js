def91550cadc9e4a612377bd2f1a20a30ec880bb
ConstruexDevelopment <149097574+ConstruexDevelopment@users.noreply.github.com>
2024-06-28 11:17:19 -0500

    apis back
    apis back

--- ProyectosDocker/index.js
+++ ProyectosDocker/index.js
@@ -1,25 +1,46 @@
-import express from 'express'
-import mongoose from 'mongoose'
+// Importar dependencias
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-const Animal = mongoose.model('Animal', new mongoose.Schema({
+// Crear esquema de Mongoose
+const animalSchema = new mongoose.Schema({
   tipo: String,
   estado: String,
-}))
+});
 
-const app = express()
+const Animal = mongoose.model('Animal', animalSchema);
 
-mongoose.connect('mongodb://migue:password@localhost:27017/miapp?authSource=admin')
+// Crear aplicación de Express
+const app = express();
 
+// Habilitar CORS
+app.use(cors({
+  origin: 'http://localhost:3000', // Cambia esto por el origen de tu frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+}));
+
+// Conectar a MongoDB
+mongoose.connect('mongodb://migue:password@localhost:27017/miapp?authSource=admin', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Rutas de Express
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando... chanchitos...');
   const animales = await Animal.find();
-  return res.send(animales)
-})
+  return res.send(animales);
+});
 
 app.get('/crear', async (_req, res) => {
-  console.log('creando...')
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
-  return res.send('ok')
-})
+  console.log('creando...');
+  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' });
+  return res.send('ok');
+});
 
-app.listen(3000, () => console.log('listening...'))
+// Iniciar el servidor
+app.listen(3001, () => {
+  console.log('listening...');
+});
